8bafe7a1087b1d6bf61a8b6a0268e95f718dd34b
HarmeshBhambra <harmeshbhambra@wireless-s0-so-150-35-200.uchicago.edu>
2015-10-23 16:03:49 -0500

    Class exercise

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,49 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def sqrt
+    # Parameters: {"thenumber"=>"1"}
+
+    @number = params[:thenumber].to_f
+
+    @answer = @number ** 0.5
+
+    render("sqrt.html.erb")
+  end
+
+  def random_number
+    # Parameters: {"lower"=>"50", "upper"=>"100"}
+
+    @lower = params[:lower].to_f
+
+    @upper = params[:upper].to_f
+
+    @random = rand(@lower...@upper).to_i
+
+
+    render("random_number.html.erb")
+
+  end
+
+  def payment
+    # Parameters: {"year"=>"30", "principal"=>"250000", "apr"=>"4"}
+
+    @years = params[:year].to_f
+
+    @apr = params[:apr].to_f
+
+    @principal = params[:principal].to_f
+
+    payments = params[:year].to_f * 12
+
+    effective_r = params[:apr].to_f / 1200
+
+    denominator = 1 - ((1 + effective_r) ** (payments * -1))
+
+    @monthly_payment = params[:principal].to_f * effective_r / denominator
+
+    render("payment.html.erb")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:thenumber", { :controller => "calculations", :action => "sqrt" })
+
+  get("/random_number/:lower/:upper", { :controller => "calculations", :action => "random_number" })
+
+  get("/payment/:year/:principal/:apr", { :controller => "calculations", :action => "payment" })
 end
